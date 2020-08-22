0dca2a7461263898a04a971373198893066d4d60
Brian Runck <brian.c.runck@gmail.com>
2020-08-21 23:38:18 -0400

    Clean up UI test

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/IssueXXX.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/IssueXXX.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Xamarin.Forms.CustomAttributes;
+﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
 #if UITEST
@@ -18,15 +17,27 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Github, 13000, "Issue Description", PlatformAffected.iOS)]
 	public class IssueXXX : TestContentPage
 	{
+		// When the page appears, it has already been pushed onto the navigation stack.
+		// This will reproduce the problem, which is that on iOS, when the page is not the first one on the stack,
+		// the AutomationId for the toolbar button will not exist. This does not happen if the page is the first one on the stack.
+
 		protected override void Init()
 		{
 			Content = new StackLayout
 			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
 				Children =
 				{
 					new Label
 					{
-						Text = "Root Page",
+						Text = "A Page",
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.Center
+					},
+					new Label
+					{
+						Text = "This can only be tested by running the UI test.",
 						HorizontalOptions = LayoutOptions.Center,
 						VerticalOptions = LayoutOptions.Center
 					}
@@ -35,39 +46,17 @@ namespace Xamarin.Forms.Controls.Issues
 
 			ToolbarItems.Add(new ToolbarItem
 			{
-				AutomationId = "FirstToolbarButtonAutomationId",
-				Text = "FirstToolbarButton"
+				AutomationId = "ToolbarButtonAutomationId",
+				Text = "ToolbarButton"
 			});
-
-			var testPage = new ContentPage
-			{
-				Content = new StackLayout
-				{
-					Children =
-					{
-						new Label
-						{
-							Text = "Second Page",
-							HorizontalOptions = LayoutOptions.Center,
-							VerticalOptions = LayoutOptions.Center
-						}
-					}
-				}
-			};
-			testPage.ToolbarItems.Add(new ToolbarItem
-			{
-				AutomationId = "SecondToolbarButtonAutomationId",
-				Text = "SecondToolbarButton"
-			});
-			_ = Navigation.PushAsync(testPage);
 		}
 
 #if UITEST
 		[Test]
 		public void IssueXXXTest()
 		{
-			RunningApp.WaitForElement(q => q.Text("Second Page"));
-			var result = RunningApp.Query(q => q.Marked("SecondToolbarButtonAutomationId"));
+			RunningApp.WaitForElement(q => q.Text("A Page"));
+			var result = RunningApp.Query(q => q.Marked("ToolbarButtonAutomationId"));
 			Assert.IsNotEmpty(result);
 		}
 #endif
